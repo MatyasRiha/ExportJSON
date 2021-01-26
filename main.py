812b30a2d15dd5800153cb05a38072a10d14593a
Matyas Riha <65078410+MatyasRiha@users.noreply.github.com>
2021-01-26 10:42:10 +0100

    Vytvořeno pomocí služby Colaboratory

---
+++ main.py
@@ -0,0 +1,45 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "Untitled0.ipynb",
+      "provenance": [],
+      "collapsed_sections": [],
+      "authorship_tag": "ABX9TyMuWe+BfnlO1+YR2XPGyAIy"
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "lmnsH_iSEPSu"
+      },
+      "source": [
+        "import numpy as np"
+      ],
+      "execution_count": null,
+      "outputs": []
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "uAQ9iVKKEVox"
+      },
+      "source": [
+        "\r\n",
+        "\r\n",
+        "\r\n",
+        "\r\n",
+        "\r\n",
+        "\r\n"
+      ],
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
